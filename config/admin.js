62a272a5e126e8f9efdad8619933826e3ff0db2a
rockxzion <rock_zion@outlook.com>
2023-07-14 14:12:07 +0100

    update config

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env("ADMIN_JWT_SECRET"),
+  },
+
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "d9b0df66ff97a666027e665707b4e3e7"),
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,13 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS"),
-  },
-  apiToken: {
-    salt: env("API_TOKEN_SALT", "d9b0df66ff97a666027e665707b4e3e7"),
-  },
-  auth: {
-    secret: env("ADMIN_JWT_SECRET", "77b2c87dbab4e1697bec244226fbd1b3"),
+    keys: env.array('APP_KEYS')
   },
 });
